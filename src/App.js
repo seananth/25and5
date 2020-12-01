43f3860bc72414f980b67f1a406fe04eee607c47
seananth <seansafstrom@gmail.com>
2020-12-01 19:58:09 +0100

    added count down timer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Countdown from './components/Countdown'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  let timeValue = 1;
+
+  return <div className="App">
+    <Countdown initialMinute={timeValue}/>
+  </div>;
 }
 
 export default App;
+
