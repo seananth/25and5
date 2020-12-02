9e13154304c067079258f12189fe9882a4f1e1de
seananth <seansafstrom@gmail.com>
2020-12-02 17:48:13 +0100

    added form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import { useState, useEffect } from "react";
 import "./App.css";
-import Countdown from './components/Countdown'
+import Countdown from "./components/Countdown";
 
 function App() {
-
   let timeValue = 1;
 
-  return <div className="App">
-    <Countdown initialMinute={timeValue}/>
-  </div>;
+  return (
+    <div className="App">
+      <Countdown initialMinute={timeValue} />
+      <form className="form">
+        <div className="minutes">
+          <label for="minutes">Work for</label>
+          <input id="minutes" placeholder="25"></input>
+          <label for="minutes">Minutes</label>
+        </div>
+        <div className="seconds">
+          <label for="seconds">Chill for </label>
+          <input id="seconds" placeholder="5"></input>
+          <label for="seconds">Minutes</label>
+        </div>
+        <button>Start</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
-
