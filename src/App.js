61f9c0110a40fb03fe51445495e6ca064a74975c
seananth <seansafstrom@gmail.com>
2020-12-03 20:47:38 +0100

    input update state on change

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 import Countdown from "./components/Countdown";
 
 function App() {
-  let timeValue = 1;
+  const [inputTime, setInputTime] = useState();
+
+  const handleInputChange = (e) => {
+    setInputTime(e.target.value);
+  };
+
+  if (inputTime > 0) {
+    return <Countdown initialMinute={inputTime} />;
+  }
 
   return (
     <div className="App">
-      <Countdown initialMinute={timeValue} />
-      <form className="form">
-        <div className="minutes">
-          <label for="minutes">Work for</label>
-          <input id="minutes" placeholder="25"></input>
-          <label for="minutes">Minutes</label>
-        </div>
-        <div className="seconds">
-          <label for="seconds">Chill for </label>
-          <input id="seconds" placeholder="5"></input>
-          <label for="seconds">Minutes</label>
-        </div>
-        <button>Start</button>
-      </form>
+      <div>
+        <input type="text" name="input" onChange={handleInputChange} />
+        <button type="submit">start</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+//pomodoro clock
+
+//Sudo code
+
+//get user input and store in state on click
+//pass state value to <countdown /> onClick
+//hide input until timer runs out
