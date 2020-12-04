ce58b73f3a816b2de401604117f622f85f88b8d5
seananth <seansafstrom@gmail.com>
2020-12-04 17:20:44 +0100

    deleted everything and starting over with an actuall plan..

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Simple productivity app that helps time my school work
+
+Pomodoro clock.
+
+Things i need to do
+
--- src/App.js
+++ src/App.js
@@ -3,32 +3,17 @@ import React, { useEffect, useState } from "react";
 import Countdown from "./components/Countdown";
 
 function App() {
-  const [inputTime, setInputTime] = useState();
 
-  const handleInputChange = (e) => {
-    setInputTime(e.target.value);
-  };
 
-  if (inputTime > 0) {
-    return <Countdown initialMinute={inputTime} />;
-  }
+
 
   return (
     <div className="App">
       <div>
-        <input type="text" name="input" onChange={handleInputChange} />
-        <button type="submit">start</button>
+
       </div>
     </div>
   );
 }
 
 export default App;
-
-//pomodoro clock
-
-//Sudo code
-
-//get user input and store in state on click
-//pass state value to <countdown /> onClick
-//hide input until timer runs out
